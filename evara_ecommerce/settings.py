de5b3a5f3ecbab2db856589fa1471c85b203d389
xradeel <xradeel7@gmail.com>
2024-07-27 21:57:49 +0500

    settings templates and basic urls

--- evara_ecommerce/settings.py
+++ evara_ecommerce/settings.py
@@ -78,8 +78,14 @@ WSGI_APPLICATION = 'evara_ecommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'evara_django',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
+
     }
 }
 
